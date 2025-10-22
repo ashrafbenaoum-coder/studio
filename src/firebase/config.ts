6377dced05f1d790e9f67bfa6845897bbd45509c
ACHRAF BENAOUM <ashrafbenaoum@gmail.com>
2025-10-22 16:46:51 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9554318450-812a8",
+  "appId": "1:86980348597:web:13b680614b93b46c9accfc",
+  "apiKey": "AIzaSyC0hcETXQYJzqVlJqYTsV6PizfssOtBKzc",
+  "authDomain": "studio-9554318450-812a8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "86980348597"
+};
